ec2a1b4c4f2b63830fa8e5728276676969f2e566
Vato Loladze <a.loladze16@gmail.com>
2023-03-16 12:40:00 +0400

    added redirection for / route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import { Route, Routes, Link } from 'react-router-dom'
+import { Route, Routes, Link, Navigate } from 'react-router-dom'
 import './App.css'
 import './BootstrapOverride.css'
 import PlanetShowcase from './components/PlanetShowcase'
@@ -12,6 +12,7 @@ function App() {
     <>
     <NavigationButtons/>
     <Routes>
+      <Route path='/' element={ <Navigate to="/Earth"/> }/>
       <Route path='/:name' element={<PlanetShowcase/>}/>
     </Routes>
 
